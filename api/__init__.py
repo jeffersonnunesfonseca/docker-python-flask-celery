9a6849b036b801e665f6aca7ffdda51a84462650
Jeff <jefferson@chavesnamao.com.br>
2021-10-17 12:04:32 -0300

    (chore) habilitando begub query no flask

--- api/__init__.py
+++ api/__init__.py
@@ -8,6 +8,7 @@ pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
+app.config["SQLALCHEMY_ECHO"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:12345@192.168.15.10:3306/webserver"
 db = SQLAlchemy(app)
 from api import routes
